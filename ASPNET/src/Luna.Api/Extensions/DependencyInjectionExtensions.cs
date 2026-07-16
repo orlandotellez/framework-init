f291a4d6bd17dc55bffeabd9b896040b5bdbf8a0
orlandotellez <espadachinkinno@gmail.com>
2026-07-16 10:37:52 -0600

    feat: agregar plantilla inicial de aspnet

---
+++ ASPNET/src/Luna.Api/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,31 @@
+using Luna.Api.Helpers;
+using Luna.Application.Features.Auth;
+using Luna.Application.Common.Interfaces;
+using Luna.Infrastructure.Persistence.Repositories;
+using Luna.Infrastructure.Services;
+
+namespace Luna.Api.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Helpers
+        services.AddScoped<CookieHelper>();
+
+        // Services Application
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IAccountRepository, AccountRepository>();
+        services.AddScoped<ISessionRepository, SessionRepository>();
+
+
+        // Services Infrastructure
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IPasswordService, PasswordService>();
+
+        // Repositories
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        return services;
+    }
+}
